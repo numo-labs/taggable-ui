6fdb630d8cd0807443bb6d81ec803440a7232669
Nikhila Ravi <nikkiravi92@gmail.com>
2016-03-24 15:33:14 +0000

    adds props to search input component

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,17 +1,25 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 
 require('./styles.css');
 class SearchBar extends Component {
   render () {
+    const { handleSubmit, onChangeText } = this.props;
     return (
-      <form className='search'>
-        <span><input type='search' className='search__input search__input--rounded' placeholder='Search by id or displayName..' /></span>
+      <form className='search' onSubmit={handleSubmit}>
+        <input
+          type='search'
+          className='search__input search__input--rounded'
+          placeholder='Search by id or displayName..'
+          onChange={onChangeText}
+        />
       </form>
     );
   }
 }
 
 SearchBar.propTypes = {
+  handleSubmit: PropTypes.func,
+  onChangeText: PropTypes.func
 };
 
 export default SearchBar;
