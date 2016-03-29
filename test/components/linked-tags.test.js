6ec961d46470f9411789274e3071d783be3284e2
Jack Carlisle <jackcarlisle@hotmail.co.uk>
2016-03-29 10:08:26 +0100

    updates tests for the linked tags component

--- test/components/linked-tags.test.js
+++ test/components/linked-tags.test.js
@@ -9,13 +9,19 @@ describe('Components', function () {
   const children = wrapper.children().nodes;
   describe('<LinkedTags />', function () {
     it('should render our LinkedTags component', function (done) {
-      expect(children).to.have.length(1);
+      expect(children).to.have.length(2);
       done();
     });
     it('should render our <TagList /> as the first child', function (done) {
       const firstChild = children[0].type;
       const TagList = wrapper.find('TagList').node.type;
       expect(firstChild).to.deep.equal(TagList);
+      done();
+    });
+    it('should render our <SearchBar /> as the second child', function (done) {
+      const secondChild = children[1].type;
+      const SearchBar = wrapper.find('SearchBar').node.type;
+      expect(secondChild).to.deep.equal(SearchBar);
       done();
     });
   });
