b0bc6a61c7715e6598afd25b31e5997329db4b00
Nikhila Ravi <nikkiravi92@gmail.com>
2016-04-12 15:53:47 +0100

    Adds comments to tag actions

--- src/actions/tag.js
+++ src/actions/tag.js
@@ -10,63 +10,85 @@ export function search () {
   return { type: types.SEARCH };
 }
 
-/*
+/**
 * onChangeText function for the search input component
-* option is either 'tag' or 'parent'
+* @param {String} - the search string
+* @param {String} - option is either 'tag' or 'parent'. This corresponds to the
+* search field in either the search pane (search for a 'tag'), or the view-pane
+* (search for a 'parent' tag)
 */
 export function setSearchString (text, option) {
   return { type: types.SET_SEARCH_STRING, text, option };
 }
 
-/*
+/**
 * onClick function for the tags in the search results
+* @param {String} - id of the selected tag
 */
 export function setSelectedTagFromSearch (id) {
   return { type: types.SET_SELECTED_TAG_FROM_SEARCH, id };
 }
 
-/*
+/**
 * Function to set the inSearch state to true to show a loading spinner while
 * results are being fetched
+* @param {String} - id of the selected tag
 */
 
 export function busySearching (option) {
   return { type: types.BUSY_SEARCHING, option };
 }
 
-/*
+/**
 * Function to add a parent linked tag to the currently selected tag
+* @param {String} - id of the selected tag
 */
 
 export function addParentTag (id) {
   return { type: types.ADD_PARENT_TAG, id };
 }
 
-/*
-* Function to remove a parent linked tag to the currently selected tag
+/**
+* Function to remove a parent tag of the currently selected tag
+* @param {String} - id of the selected tag
+* @param {String} - option is either 'tag' or 'parent'. This corresponds to the
+* search field in either the search pane (search for a 'tag'), or the view-pane
+* (search for a 'parent' tag)
 */
 
 export function removeParentTag (id) {
   return { type: types.REMOVE_PARENT_TAG, id };
 }
 
-/*
+/**
 * Function to save the retrieved results from graphql to redux store
+* @param {String} - id of the selected tag
 */
 
 export function setSearchResults (searchResults, option) {
   return { type: types.SET_SEARCH_RESULTS, searchResults, option };
 }
 
-/*
+/**
 * Function to set the queryType and tagType of the query
+* @param {String} - queryType e.g. QUERY_ID, QUERY_DISPLAYNAME
+* @param (String) - tagType e.g. 'GEO', 'MARKETING', 'AMENITY'
+* @param {String} - option is either 'tag' or 'parent'. This corresponds to the
+* search field in either the search pane (search for a 'tag'), or the view-pane
+* (search for a 'parent' tag)
 */
 
 export function setTagTypeAndQueryType (queryType, tagType, option) {
   return { type: types.SET_TAG_TYPE_AND_QUERY_TYPE, tagType, queryType, option };
 }
-/*
-*  Function to retrieve tags based on a searchString, queryType and tagType
+
+/**
+* Function to retrieve tags by launching a graphql query
+* @param {String} - start - offset index for pagination
+* @param (String) - size -  number of tags to retrieve
+* @param {String} - option - either 'tag' or 'parent'. This corresponds to the
+* search field in either the search pane (search for a 'tag'), or the view-pane
+* (search for a 'parent' tag)
 */
 
 export function fetchTags (start, size, option) {
@@ -106,7 +128,7 @@ export function saveConfiguration () {
 }
 
 /*
-* Function that will remove a specific value
+* Function that will remove a specific value of a key in the metadata array
 */
 
 export function deleteValue (metaIndex, index) {
